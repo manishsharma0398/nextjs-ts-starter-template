8f86e9d3e22b3247b310ab08817f162611c9256f
Manish Sharma <manish.sharma0398@gmail.com>
2024-05-12 17:42:59 +0530

    feat: jest integrtion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Hi from '@components/Hi';
+import HelloWorld from '@components/HelloWorld';
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Home page</h1>
-      <Hi />
+      <HelloWorld />
     </div>
   );
 };
--- src/components/Hi.tsx
+++
@@ -1,5 +0,0 @@
-const Hi = () => {
-  return <h1>hi</h1>;
-};
-
-export default Hi;
