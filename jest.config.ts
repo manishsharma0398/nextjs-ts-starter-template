efeb394a418a3d7ccc17ce488a691615d0de4140
Manish Sharma <manish.sharma0398@gmail.com>
2024-05-12 20:58:01 +0530

    fix: type check while running tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,14 @@
  * https://jestjs.io/docs/configuration
  */
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const nextJs = require('next/jest');
+import type { Config } from 'jest';
+import nextJs from 'next/jest';
 
 const createJestConfig = nextJs({
   dir: './',
 });
 
-const config = {
+const config: Config = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -72,7 +72,11 @@ const config = {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.jest.json',
+    },
+  },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -200,4 +204,4 @@ const config = {
   // watchman: true,
 };
 
-module.exports = createJestConfig(config);
+export default createJestConfig(config);
